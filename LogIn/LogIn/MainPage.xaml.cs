e7a2be2118e90d58dcd3149bed8af768d988def1
Usuario <Usuario@DESKTOP-A4NCRUK>
2021-08-19 20:03:37 -0400

    Add project files.

---
+++ LogIn/LogIn/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace LogIn
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "Admin" && txtContraseña.Text == "123")
+            {
+                DisplayAlert("Saludos!", "Vienvenido: Admin", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Alerta", "No pueden existir campos vacíos", "Ok");
+            }
+        }
+
+        
+    }
+
+}
